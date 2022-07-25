527fbf2bfdbb724caddbc639ac71bbaa5135b8e3
Lauren Lee <86620096+laurenkiyomi@users.noreply.github.com>
2022-07-25 11:26:10 -0700

    Set up  gohike components directory

---
+++ gohike-components/demo/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from 'gohike-components/LoadingIcon';
+
+function App() {
+  return (
+    <div className="App">
+        <Button>Button component</Button>
+    </div>
+  );
+}
+
+export default App;
