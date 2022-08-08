511cec97859ca40ba5b1d19ea353a258e342d6c4
Lauren Lee <86620096+laurenkiyomi@users.noreply.github.com>
2022-08-08 09:19:37 -0700

    Changed url

--- gohike-api/server.js
+++ gohike-api/server.js
@@ -2,6 +2,6 @@ const app = require("./app")
 
 const port = process.env.PORT || 3001
 
-app.listen(port, () => {
-  console.log(`🚀 Server listening on port ` + port)
+app.listen("https://stark-hamlet-74597.herokuapp.com", () => {
+  console.log(`🚀 Server listening on ` + "https://stark-hamlet-74597.herokuapp.com")
 })
